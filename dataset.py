c9ac2e24c1af95264a33159f4f645d5b334558e0
Jklein64 <jklein.64@verizon.net>
2022-11-19 14:07:40 -0500

    Make dataset treat each image as new entry

--- dataset.py
+++ dataset.py
@@ -5,12 +5,17 @@ from PIL import Image
 import os
 
 class CarDrivingDataset(torch.utils.data.Dataset):
-    """ Dataset for car steering angle, throttle, brake, and speed. Assumes that the data is in a folder data/ relative to the python root. """
+    """ Dataset for car steering angle. Assumes that the data is in a folder data/ relative to the python root. """
     
     labels: pd.DataFrame
 
     def __init__(self, transform=None) -> None:
-        self.labels = pd.read_csv("./data/labels.csv")
+        labels = pd.read_csv("./data/labels.csv")
+        # treat each camera input as a separate row
+        self.labels = pd.concat(
+            labels.get([i, "steering angle"]).rename({ i, "image" }, axis=1)
+            for i in ("left image", "center image", "right image")) 
+
         self.transform = transform
 
 
@@ -21,10 +26,10 @@ class CarDrivingDataset(torch.utils.data.Dataset):
         # build result
         result = {}
         item = self.labels.iloc[index]
-        for key in ("left image", "center image", "right image"):
-            file = os.path.join(".", "data", "images", item.loc[key])
-            image = np.array(Image.open(file))
-            result[key] = image
+
+        file = os.path.join(".", "data", "images", item.loc["image"])
+        image = np.array(Image.open(file))
+        result["image"] = image
             
         # only actually get the steering angle value
         angle = item.loc["steering angle"]
