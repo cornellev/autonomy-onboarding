22f9350be33f52607d391055921b8b89149908b0
jak532 <jak532@cornell.edu>
2023-01-31 20:41:37 -0500

    Save every epoch

--- main.py
+++ main.py
@@ -5,20 +5,27 @@ from torchvision.transforms import Resize, Compose, ToTensor
 from network import TurnNet
 from dataset import CarDrivingDataset
 
+# path for checkpoints
+PATH = "checkpoint.pt"
 
 transform = Compose([ToTensor(), Resize((66, 200))])
 # see https://discuss.pytorch.org/t/dictionary-in-dataloader/40448 for proper use
 loader = DataLoader(CarDrivingDataset(transform=transform), batch_size=8, shuffle=True)
 
 model = TurnNet()
-model.train()
 model.to("cpu")
 
 # https://towardsdatascience.com/linear-regression-with-pytorch-eb6dedead817
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-prevweights = None
+# load from the checkpoint file
+checkpoint = torch.load(PATH)
+model.load_state_dict(checkpoint['model_state_dict'])
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+loss = checkpoint['loss']
+
+model.train()
 
 # https://github.com/christianversloot/machine-learning-articles/blob/main/how-to-create-a-neural-network-for-regression-with-pytorch.md
 # Run the training loop
@@ -54,8 +61,16 @@ for epoch in range(0, 5): # 5 epochs at maximum
         # Print statistics
         current_loss += loss.item()
         if i % 10 == 0:
-            print(f'Loss after mini-batch {i + 1:5d}: {current_loss / 10:.10f} for steering angle {outputs.tolist()}')
+            #print(f'Loss after mini-batch {i + 1:5d}: {current_loss / 10:.10f} for steering angle {outputs.tolist()}')
             current_loss = 0.0
+
+    # save to checkpoint file
+    print(f"Saving data from epoch {epoch + 1}")
+    torch.save({
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "loss": loss
+    }, PATH)
 
 # Process is complete.
 print('Training process has finished.')
