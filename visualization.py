c6ee17b3950e66b756a7da1ca6884ea0e9962db4
Jklein64 <jklein.64@verizon.net>
2023-02-07 21:48:54 -0500

    Get angle showing working

--- visualization.py
+++ visualization.py
@@ -1,65 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def show_angle(image, angle):
-    """ Given an input image (as a numpy array) and associated steering angle (between -1 and 1)
-    
-    Returns a new image with a vector plotted on it representing the associated steering angle."""
-    
-    #create figure and axis objects, plot image
-    fig, ax = plt.subplots(1,figsize=(10,10))
-    ax.imshow(image, cmap='gray')
-
-
-    #is the conversion correct and does this length work?
-    angle = angle * 180
+def show_angle(image, angle, color=(1, 0, 0)):
+    """ Given an input image (as a numpy array) and associated steering angle (between -1 and 1), returns a new image with a vector plotted on it representing the associated steering angle. Uses color, which is optional and should be an array-like of length 3."""
+    # angle is -1 to 1 and gets mapped to
+    # -25 to 25 in the simulation, FACING UP
+    angle = angle * 25
     length = 50
 
     #plot steering angle vector
-    ax.quiver(image.shape[0]/2, image.shape[1]/2, length*np.cos(np.deg2rad(angle)), -(length)*np.sin(np.deg2rad(angle)), angles='xy', scale_units='xy', scale=1, color='red')
-    
-    ax.set_axis_off()
-    ax.set_xlim(0, image.shape[1])
-    ax.set_ylim(0, image.shape[0])
-    
-    #draws image and vector
-    fig.canvas.draw()
-    image = np.array(fig.canvas.renderer._renderer)
-    plt.close(fig)
-    
-    #returns image with plotted vector
-    return image
+    h, w = np.shape(image)[0:2]
+    u = length * np.cos(np.deg2rad(90 - angle))
+    v = length * -np.sin(np.deg2rad(90 - angle))
+
+    t = np.linspace(0, 1, 100)
+    x = (w / 2) + u * t
+    y = (h) + v * t
+    points = np.stack((y, x)).T
+    points = points.astype(int)
+    points = np.unique(points, axis=0)
+    # correct for linspace bounds
+    points = points[points.T[0] < h - 1]
+    points = points[points.T[1] < w - 1]
+    # make the line
+    tmp = np.array(image)
+    tmp[tuple(points.T)] = color
+    # make the line thicker
+    tmp[tuple((points + [1, 0]).T)] = color
+    tmp[tuple((points + [0, 1]).T)] = color
+    # place tmp where tmp is non-empty
+    return tmp
 
 
-def show_2_angles(image, angle1, angle2):
-    """ Given an input image (as a numpy array) and two steering angles (between -1 and 1).
-    
-    Returns a new image with 2 vectors plotted on it representing both steering angles."""
-
-    #create figure with size (10,10) and axis 
-    fig, ax = plt.subplots(1, figsize=(10, 10))
-    ax.imshow(image, cmap='gray')
-
-    #is the conversion correct and does this length work?
-    angle1= angle1*180
-    angle2= angle2*180
-    length = 50
-
-    #plot vectors
-    ax.quiver(image.shape[0]/2, image.shape[1]/2, length*np.cos(np.deg2rad(angle1)), -(length)*np.sin(np.deg2rad(angle1)), color='red', angles='xy', scale_units='xy', scale=1, headwidth=5)
-    ax.quiver(image.shape[0]/2, image.shape[1]/2, length*np.cos(np.deg2rad(angle2)), -(length)*np.sin(np.deg2rad(angle2)), color='blue', angles='xy', scale_units='xy', scale=1, headwidth=5)
-    
-    ax.set_axis_off()
-    ax.set_xlim(0, image.shape[0])
-    ax.set_ylim(0, image.shape[1])
-
-    #draws image and vectors
-    fig.canvas.draw()
-    image = np.array(fig.canvas.renderer._renderer)
-    plt.close(fig)
-
-    #return image with plotted vectors
-    return image
+def show_2_angles(image, angle_pred, angle_truth):
+    """ Given an input image (as a numpy array) and two steering angles (between -1 and 1), returns a new image with 2 vectors plotted on it representing both steering angles. """
+    # color prediction in red
+    tmp = show_angle(image, angle_pred, color=(1, 0, 0))
+    # color truth in green
+    tmp = show_angle(tmp, angle_truth, color=(0, 1, 0))
+    return tmp
 
 
 if __name__ == "__main__":
@@ -72,10 +51,7 @@ if __name__ == "__main__":
     row = data[658]
     filename = f"data/images/{row[0]}"
     image = np.array(Image.open(filename)) / 255.
-    angle = row[-2]
+    angle = float(row[-2])
 
-    visual = show_angle(image, angle)
-    
-    Image.fromarray((image * 255).astype(np.uint8)).show()
-    print(angle)
-    ...
+    visual = show_2_angles(image, np.random.random()*2-1, angle)
+    Image.fromarray((visual * 255).astype(np.uint8)).show()
